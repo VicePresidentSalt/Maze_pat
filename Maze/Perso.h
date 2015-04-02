c81c9cac609368fd3ad0774b8348179495cb669a
hcfranck <hc_throwdown@hotmail.com>
2015-04-01 22:08:18 -0400

    push

--- Maze/Perso.h
+++ Maze/Perso.h
@@ -1,1 +1,36 @@
 #pragma once
+#include "Position.h"
+#include "Orientation.h"
+
+class Perso
+{
+	Orientation direction_;
+	Position position_;
+
+	void setDir(Orientation o) throw()
+	{
+		direction_ = o ;
+	}
+
+public:
+	Perso(const Position &p = Position(), const Orientation o = Nord) throw()
+		: position_(p), direction_(o)
+	{
+		
+	}
+
+	Orientation GetDir() const throw()
+	{
+		return direction_;
+	}
+
+	Position GetPosition() const
+	{
+		return position_;
+	}
+
+	void Gauche() throw()
+	{
+		setDir(PivoterGauche(GetDir());
+	}
+};
